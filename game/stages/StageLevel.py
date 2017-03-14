aaebc3bb93be09108f2b446778bc5222268e59ee
pglandon78 <pglandon78@gmail.com>
2017-03-14 18:40:05 +0100

    - Résolution des bugs de Grid2 - Ajout de la téléportation "in-game"

--- game/stages/StageLevel.py
+++ game/stages/StageLevel.py
@@ -1,22 +1,20 @@
 import pygame.mixer
 
 from api.Map import Map
-from api.StageAutoManage import StageAutoManage
-from game.actors.ActorCollidable import ActorCollidable
 from game.actors.ActorPlayer import ActorPlayer
 from game.actors.ActorSpawnpoint import ActorSpawnpoint
-from game.actors.ActorWall import ActorWall
 from game.stages.StageHandleConsole import StageHandleConsole
+from game.utils.Constants import EVENT_TP
 
 
 class StageLevel(StageHandleConsole):
-    def __init__(self, level = 0):
+    def __init__(self, map="level_0"):
         super().__init__()
         pygame.mixer.music.load("music/CityofIntrigues.wav")
         pygame.mixer.music.play()
         pygame.mixer.music.queue("music/CommandingtheFury.wav")
 
-        self.map = Map.load("ressources/level_" + str(level))
+        self.map = Map.load_save(map)
         spawnpoint = self.map.get_actor(ActorSpawnpoint)
         if spawnpoint is not None:
             player = ActorPlayer()
@@ -30,3 +28,13 @@ class StageLevel(StageHandleConsole):
 
     def run(self):
         super().run()
+
+    def handle_userevent(self, event):
+        if event.name == EVENT_TP:
+            actor = event.actor
+            actor.rect.x = int(event.spawn_pos.x)
+            actor.rect.y = int(event.spawn_pos.y)
+            self.map.remove_actor(actor)
+            self.map.save()  # devrait appeler save_in_game mais bon en attendant...
+            self.map = Map.load_save(event.map_name)
+            self.map.add_actor(event.actor)
